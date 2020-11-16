4d55505c21ea3782cfd80b1877aef5a4be1df9fc
Death <M.26A20.M@Gmail.com>
2020-11-17 00:54:33 +0330

    Added App.vue and modified project to recognize it

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+	<section>
+		<h2>Friends</h2>
+		<ul>
+			<li></li>
+		</ul>
+	</section>
+</template>
+
+<script>
+export default {
+	data() {
+		return {
+			friends: [
+				{
+					id: "manuel",
+					name: "Manuel Lorenz",
+					phone: "123 456 7890",
+					email: "manuel@localhost.com",
+				},
+				{
+					id: "jolie",
+					name: "Jolie Jones",
+					phone: "098 765 4321",
+					email: "jolie@localhost.com",
+				},
+			],
+		};
+	},
+};
+</script>
+
+<style></style>
