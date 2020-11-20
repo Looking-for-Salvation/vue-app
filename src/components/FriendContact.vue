ed12818bd72a216da59ecfc044e26325c0c23582
Death <M.26A20.M@Gmail.com>
2020-11-20 22:27:48 +0330

    Modified FriendContact component

--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -1,59 +1,59 @@
 <template>
-	<li>
-		<h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
-		<button @click="toggleDetails">{{ detailsAreVisible ? "Hide" : "Show" }} Details</button>
-		<button @click="toggleFavorite">Toggle Favorite</button>
-		<ul v-if="detailsAreVisible">
-			<li><strong>Phone : </strong>{{ phoneNumber }}</li>
-			<li><strong>Email : </strong>{{ emailAddress }}</li>
-		</ul>
-	</li>
+   <li>
+      <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+      <button @click="toggleDetails">{{ detailsAreVisible ? "Hide" : "Show" }} Details</button>
+      <button @click="toggleFavorite">Toggle Favorite</button>
+      <ul v-if="detailsAreVisible">
+         <li><strong>Phone : </strong>{{ phoneNumber }}</li>
+         <li><strong>Email : </strong>{{ emailAddress }}</li>
+      </ul>
+   </li>
 </template>
 
 <script>
 export default {
-	name: "Friend Contact",
-	// Props as Array
-	// props: [
-	// 	'name',
-	// 	'phoneNumber',
-	// 	'emailAddress'
-	// ],
-	props: {
-		id: {
-			type: String,
-			required: true,
-		},
-		name: {
-			type: String,
-			required: true,
-		},
-		phoneNumber: {
-			type: String,
-			required: true,
-		},
-		emailAddress: {
-			type: String,
-			required: true,
-		},
-		isFavorite: {
-			type: Boolean,
-			required: false,
-			default: false,
-		},
-	},
-	data() {
-		return {
-			detailsAreVisible: false,
-		};
-	},
-	methods: {
-		toggleDetails() {
-			this.detailsAreVisible = !this.detailsAreVisible;
-		},
-		toggleFavorite() {
-			this.$emit("toggle-favorite", this.id);
-		},
-	},
+   name: "Friend Contact",
+   // Props as Array
+   // props: [
+   // 	'name',
+   // 	'phoneNumber',
+   // 	'emailAddress'
+   // ],
+   props: {
+      id: {
+         type: String,
+         required: true,
+      },
+      name: {
+         type: String,
+         required: true,
+      },
+      phoneNumber: {
+         type: String,
+         required: true,
+      },
+      emailAddress: {
+         type: String,
+         required: true,
+      },
+      isFavorite: {
+         type: Boolean,
+         required: false,
+         default: false,
+      },
+   },
+   data() {
+      return {
+         detailsAreVisible: false,
+      };
+   },
+   methods: {
+      toggleDetails() {
+         this.detailsAreVisible = !this.detailsAreVisible;
+      },
+      toggleFavorite() {
+         this.$emit("toggle-favorite", this.id);
+      },
+   },
 };
 </script>
