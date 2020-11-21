414c2d1853679d5a41e5338b794573e8854b13af
Death <M.26A20.M@Gmail.com>
2020-11-21 11:53:22 +0330

    Added AddContact component and connect it to App.vue

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import FriendContact from "./components/FriendContact.vue";
+import AddContact from "./components/AddContact.vue";
 
 createApp(App)
-	.component("friend-contact", FriendContact)
-	.mount("#app");
+   .component("friend-contact", FriendContact)
+   .component("add-contact", AddContact)
+   .mount("#app");
